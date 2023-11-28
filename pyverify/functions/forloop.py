b03d2ca59b569f81de49208388ccc6a1ce13835a
熊超 <chao.xiong@9m-tec.com>
2023-11-28 13:38:19 +0800

    fix:.

--- pyverify/functions/forloop.py
+++
@@ -1,9 +0,0 @@
-NOT_UNPACK = 0
-LIST_UNPACK = 1
-DICT_UNPACK = 2
-
-
-def for_params(func, iteration, *, unpack=NOT_UNPACK):
-    unpack_functions = {NOT_UNPACK: func, LIST_UNPACK: lambda x: func(*x), DICT_UNPACK: lambda x: func(**x)}
-    for item in iteration:
-        unpack_functions[unpack](item)
