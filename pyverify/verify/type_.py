d578c370e637650f22d6e4f23ca06b71cbb9ed2a
熊超 <chao.xiong@9m-tec.com>
2023-12-08 16:49:21 +0800

    fix:注释

--- pyverify/verify/type_.py
+++ pyverify/verify/type_.py
@@ -135,16 +135,17 @@ class Str(RuleBase):
     multi: bool = False
     func: Union[Callable, None] = None
 
-    # Length
+    # length
     minLength: Union[int, None] = None
     maxLength: Union[int, None] = None
 
-    # Enum
+    # enum
     enum: Union[List[str], None] = None
 
-    # Pruning of the string
+    # pruning of the string
     # strip lstrip rstrip Functions corresponding to python string operations;
-    # strip_chars lstrip_chars rstrip_chars corresponds to the parameters of the above three functions.
+    # strip_chars lstrip_chars rstrip_chars corresponds to the parameters of
+    # the above three functions.
     strip: bool = False
     lstrip: bool = False
     rstrip: bool = False
@@ -152,21 +153,67 @@ class Str(RuleBase):
     lstrip_chars: Union[str, None] = None
     rstrip_chars: Union[str, None] = None
 
-    # Judge
+    # judge
+    # startswith: Determines whether the string begins with the specified
+    # character or substring.
     startswith: Union[str, None] = None
+    # endswith: Determines whether the string ends with the specified
+    # character or substring.
     endswith: Union[str, None] = None
+    # isalnum: Detects whether a string consists of letters and numbers.
     isalnum: bool = False
+    # isalpha: Detects whether a string consists of only letters.
     isalpha: bool = False
+    # isdecimal: Checks if the string contains only decimal characters.
     isdecimal: bool = False
+    # isdigit: Detects whether a string consists only of numbers.
     isdigit: bool = False
+    # isidentifier: Determines whether str is a valid identifier.
     isidentifier: bool = False
+    # islower: Checks whether the letters in a string are all lowercase letters.
     islower: bool = False
+    # isupper: Detects whether a string consists of all uppercase letters.
     isupper: bool = False
+    # isprintable: Determine whether there is anything in the string that is not
+    # visible after printing. For example: \n \t and other characters.
     isprintable: bool = False
+    # isspace: Detects whether a string consists only of Spaces.
     isspace: bool = False
+    # istitle: Detection Determines whether the first letter of all words in the
+    # string is uppercase and other letters are lowercase. There can be other
+    # characters in the string that are not letters.
     istitle: bool = False
+    # include: Detects whether the string contains the specified string.
     include: Union[str, None] = None
+    # exclude: Detects whether the string does not contain the specified string.
     exclude: Union[str, None] = None
+
+    # rewriting
+    # replace: replacement string content.
+    # replace_args: (old, new, count)
+    replace: bool = False
+    replace_args: tuple = ()
+    # capitalize: Change the first letter of the string to uppercase and
+    # the remaining letters to lowercase.
+    capitalize: bool = False
+    # title: Returns a string that satisfies the title format. That is,
+    # the first letter of all English words is capitalized and the other
+    # English letters are lowercase.
+    title: bool = False
+    # swapcase: Swap both uppercase and lowercase letters in the string
+    # str. Converts uppercase letters to lowercase letters and lowercase
+    # letters to uppercase letters in the string str.
+    swapcase: bool = False
+    # lower: Converts all uppercase letters in a string to lowercase
+    # letters.
+    lower: bool = False
+    # upper: Converts all lowercase letters in a string to uppercase
+    # letters.
+    upper: bool = False
+    # casefold: Converts all uppercase letters in a string to lowercase
+    # letters. You can also convert uppercase to lowercase in non-English
+    # languages.
+    casefold: bool = False
 
     # re
     regex: Union[str, None] = None
@@ -237,6 +284,10 @@ class Str(RuleBase):
             if self.istitle and not value.istitle():
                 # TODO:错误信息
                 raise
+
+            # replace
+            if self.replace:
+                value = value.replace(*self.replace_args)
 
         # enum
         value = self.verify_enum(key, value)
