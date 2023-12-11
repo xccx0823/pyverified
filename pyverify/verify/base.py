b820877bd56ecb4ddb665c79468b7f71bea3c3ed
熊超 <chao.xiong@9m-tec.com>
2023-12-11 14:22:21 +0800

    fix:bug

--- pyverify/verify/base.py
+++ pyverify/verify/base.py
@@ -16,7 +16,7 @@ class RuleBase:
     def execute_parse(self, key: str, value: Any):
         if self.multi:  # noqa
             if not isinstance(value, (list, set, tuple)):
-                raise ValidationError(msg.Message.multi.format(key=key, value=value))
+                raise ValidationError(msg.message.multi.format(key=key, value=value))
             _values = []
             for _value in value:
                 _value = self.common_rules_verify(key, _value)
@@ -35,12 +35,12 @@ class RuleBase:
     def verify_required(self, key: str, value: Any):
         """Check whether parameters are missing."""
         if self.required and isinstance(value, Unset):  # noqa
-            raise ValidationError(msg.Message.required.format(key=key, value=value))
+            raise ValidationError(msg.message.required.format(key=key, value=value))
 
     def verify_allow_none(self, key: str, value: Any):
         """Check whether the parameter can be None."""
         if not self.allow_none and value in self.null_values:  # noqa
-            raise ValidationError(msg.Message.allow_none.format(key=key, value=value))
+            raise ValidationError(msg.message.allow_none.format(key=key, value=value))
 
         # When allow_none is True, value is replaced with None if it is unset.
         if isinstance(value, Unset):
@@ -59,22 +59,22 @@ class RuleBase:
             try:
                 value = self.enum[value]  # noqa
             except KeyError:
-                raise ValidationError(msg.Message.enum.format(key=key, value=value, enum=self.enum))  # noqa
+                raise ValidationError(msg.message.enum.format(key=key, value=value, enum=self.enum))  # noqa
         elif isinstance(self.enum, list):  # noqa
             if value not in self.enum:  # noqa
-                raise ValidationError(msg.Message.enum.format(key=key, value=value, enum=self.enum))  # noqa
+                raise ValidationError(msg.message.enum.format(key=key, value=value, enum=self.enum))  # noqa
         return value
 
     def verify_range(self, key: str, value: Any):
         """The range of the check value."""
         if self.gt is not None and value <= self.gt:  # noqa
-            raise ValidationError(msg.Message.gt.format(key=key, value=value, gt=self.gt))  # noqa
+            raise ValidationError(msg.message.gt.format(key=key, value=value, gt=self.gt))  # noqa
         if self.gte is not None and value < self.gte:  # noqa
-            raise ValidationError(msg.Message.gte.format(key=key, value=value, gte=self.gte))  # noqa
+            raise ValidationError(msg.message.gte.format(key=key, value=value, gte=self.gte))  # noqa
         if self.lt is not None and value >= self.lt:  # noqa
-            raise ValidationError(msg.Message.lt.format(key=key, value=value, lt=self.lt))  # noqa
+            raise ValidationError(msg.message.lt.format(key=key, value=value, lt=self.lt))  # noqa
         if self.lte is not None and value > self.lte:  # noqa
-            raise ValidationError(msg.Message.lte.format(key=key, value=value, lte=self.lte))  # noqa
+            raise ValidationError(msg.message.lte.format(key=key, value=value, lte=self.lte))  # noqa
 
     def set_default_value(self, value: Any):
         """Set a default value."""
