2af52129e296c83584392ca02d5118a2d3ed7a54
熊超 <chao.xiong@9m-tec.com>
2024-02-29 14:54:50 +0800

    modify msg

--- pyverified/msg.py
+++ pyverified/msg.py
@@ -1,13 +1,10 @@
 class _Message:
     """Define check exception information."""
-    # special type
     email = '`{key}` 的值 `{value}` 不是邮箱格式。'
     phone = '`{key}` 的值 `{value}` 不是电话号码格式。'
     ipv4 = '`{key}` 的值 `{value}` 不是ipv4地址格式。'
     ipv6 = '`{key}` 的值 `{value}` 不是ipv6地址格式。'
     address = '`{key}` 的值 `{value}` 不是链接地址格式。'
-
-    # common
     required = '`{key}` 是必须的。'
     allow_none = '`{key}` 不能为空。'
     many = '校验数据必须是数组。'
@@ -17,14 +14,8 @@ class _Message:
     gte = '`{key}` 的值 `{value}` 必须大于等于 `{gte}` 。'
     lt = '`{key}` 的值 `{value}` 必须小于 `{lt}` 。'
     lte = '`{key}` 的值 `{value}` 必须小于等于 `{lte}` 。'
-
-    # type
     type = '`{key}` 的值 `{value}` 无法转化为 `{type}` 类型。'
-
-    # bool
     convert = '`{key}` 的值 `{value}` 不能转化为布尔类型。'
-
-    # str
     minLength = '`{key}` 的值 `{value}` 的长度不能小于 `{minLength}`。'
     maxLength = '`{key}` 的值 `{value}` 的长度不能大于 `{maxLength}`。'
     startswith = '`{key}` 的值 `{value}` 必须以 `{startswith}` 字符串开头。'
@@ -59,14 +50,11 @@ message = _Message
 
 class _MessageEn:
     """Define check exception information."""
-    # special type
     email = 'The value `{value}` for `{key}` is not in email format.'
     phone = 'The value `{value}` for `{key}` is not in phone number format.'
     ipv4 = 'The value `{value}` for `{key}` is not in IPv4 address format.'
     ipv6 = 'The value `{value}` for `{key}` is not in IPv6 address format.'
     address = 'The value `{value}` for `{key}` is not in URL address format.'
-
-    # common
     required = '`{key}` is required.'
     allow_none = '`{key}` cannot be empty.'
     many = 'Validation data must be an array.'
@@ -77,11 +65,7 @@ class _MessageEn:
     lt = 'The value `{value}` of `{key}` must be less than `{lt}`.'
     lte = 'The value `{value}` of `{key}` must be less than or equal to `{lte}`.'
     type = 'The value `{value}` of `{key}` cannot be converted to type `{type}`'
-
-    # bool
     convert = 'The value `{value}` of `{key}` cannot be converted to boolean type.'
-
-    # str
     minLength = 'The length of `{value}` for `{key}` must not be less than `{minLength}`.'
     maxLength = 'The length of `{value}` for `{key}` must not exceed `{maxLength}`.'
     startswith = 'The value `{value}` for `{key}` must start with the string `{startswith}`.'
